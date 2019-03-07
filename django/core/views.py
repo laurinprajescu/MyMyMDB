0945577bc99987844f36fef5b5724eddcf191726
laurinprajescu <laurinprajescu@gmail.com>
2019-03-07 21:30:02 +0000

    Add base.html

--- django/core/views.py
+++ django/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
-from django.views.generic import ListView
-
+from django.views.generic import (ListView, DetailView,)
 from core.models import Movie
+
+class MovieDetail(DetailView):
+	model: Movie
 
 class MovieList(ListView):
 	model = Movie
